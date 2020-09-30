828616cd098d3e89d5e847f8951b367f45a7746b
github username <Lukabaramishvili@gmail.com>
2020-09-30 21:58:07 +0400

    update the amount of mixes to be shown

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
 
 	return (
 		<div className="flex flex-wrap justify-between mixes ph3 ph4-l mb5">
-			{mixes?.slice(0, 6).map((mix) => (
+			{mixes?.slice(0, 15).map((mix) => (
 				<div key={mix.id} className="mix mb4">
 					<Mix name={mix.name} id={mix.key} pictures={mix.pictures} mix={mix} />
 				</div>
