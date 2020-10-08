90a45a1e491a81a08bf68de9e099ddc9929d687b
github username <Lukabaramishvili@gmail.com>
2020-10-08 15:23:35 +0400

    integrate read & write to db

--- src/components/App.js
+++ src/components/App.js
@@ -11,36 +11,36 @@ import Player from './Player';
 import Suggest from './Suggest';
 
 const App = () => {
-	return (
-		<GlobalState>
-			<Route>
-				<div className="flex-l justify-end">
-					<FeaturedMix />
-					<div className="w-50-l relative z-1">
-						<Header />
-						<Switch>
-							<Route exact path="/">
-								<Home />
-							</Route>
-							<Route path="/archive">
-								<Archive />
-							</Route>
-							<Route path="/about">
-								<About />
-							</Route>
-							<Route path="/show/:slug">
-								<Show />
-							</Route>
-							<Route path="/suggest">
-								<Suggest />
-							</Route>
-						</Switch>
-					</div>
-				</div>
-				<Player />
-			</Route>
-		</GlobalState>
-	)
+   return (
+    <GlobalState>
+        <Route>
+            <div className="flex-l justify-end">
+                <FeaturedMix />
+                    <div className="w-50-l relative z-1">
+                        <Header />
+                        <Switch>
+                            <Route exact path="/">
+                                <Home />
+                            </Route>
+                            <Route path="/archive">
+                                <Archive />
+                            </Route>
+                            <Route path="/about">
+                                <About />
+                            </Route>
+                            <Route path="/show/:slug">
+                                <Show />
+                            </Route>
+                            <Route path="/suggest">
+                                <Suggest />
+                            </Route>
+                        </Switch>
+                    </div>
+            </div>
+                <Player />
+        </Route>
+    </GlobalState>
+    )
 }
 
 export default App;
--- src/components/Suggest.js
+++ src/components/Suggest.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { db } from '../firebase';
+import SuggestedLinks from './SuggestedLinks';
 
 const Suggest = () => {
+    const [ urlItem, setUrlItem ] = useState([]);
+
+    const handleInputChange = (e) => {
+        setUrlItem(e.target.value)
+    }
+    
+    const addToDatabase = (e) => {
+    e.preventDefault();
+    db.collection('Urls')
+      .add({
+        name: urlItem
+      })
+      .then((res) => {
+        setUrlItem('');
+        alert('Successfully added to database, 🎉');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+
     return (
-        <div>
-            Your Suggestions go here!
-        </div>
+    <div>
+        <form onSubmit={addToDatabase} className="add-item-form">
+            <label htmlFor="url-item">url</label>
+                <input
+                    name="list-item"
+                    id="list-item"
+                    onChange={handleInputChange}
+                    value={urlItem}
+                    placeholder="Add Url"
+                />
+            <button className="add-item-btn">Add</button>
+        </form>
+        <SuggestedLinks />
+    </div>
     )
 }
 
-export default Suggest
+export default Suggest;
