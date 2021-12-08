cb0f6cd29e9bb44a9e5f1f8bdf8cdd254e563a8d
github username <Lukabaramishvili@gmail.com>
2021-12-08 13:37:01 +0400

    create .env for api security

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const fb = firebase.initializeApp({
-  apiKey: "AIzaSyBvQbtLYNhP5sbiNoebpbClyCHLmtE2hro",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "falafel-fm.firebaseapp.com",
   databaseURL: "https://falafel-fm.firebaseio.com",
   projectId: "falafel-fm",
   storageBucket: "falafel-fm.appspot.com",
   messagingSenderId: "898247582533",
-  appId: "1:898247582533:web:c1953589e7d5ed7f59d42c",
-  measurementId: "G-NZY4MF8JF0"
+  appId: "1:898247582533:web:7c81e24739a71f2459d42c",
+  measurementId: "${config.measurementId}"
 });
 
 let db = fb.firestore();
